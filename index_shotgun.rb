a58430ca5deeee20e563fa56374c3743795224cd
akicho8 <akicho8@gmail.com>
2017-02-08 22:56:33 +0900

    闇雲インデックスのリファクタリング

--- index_shotgun.rb
+++ index_shotgun.rb
@@ -1,33 +1,25 @@
 require "active_record"
 
 ActiveRecord::Migration.verbose = false
-ActiveRecord::Base.establish_connection(adapter: "mysql2", host: "127.0.0.1")
-ActiveRecord::Base.connection.recreate_database("__test__")
-ActiveRecord::Base.establish_connection(ActiveRecord::Base.connection_config.merge(database: "__test__"))
+ActiveRecord::Base.establish_connection(adapter: "sqlite3", database: ":memory:")
 
 ActiveRecord::Schema.define do
-  create_table :articles do |t|
-    t.string :name
-    t.string :message
-    t.timestamps
+  create_table :users do |t|
+    t.string :name, :index => true
+    t.timestamps    :index => true
 
-    t.index :id
-    t.index :name
-    t.index :message
-    t.index [:message, :name]
-    t.index [:id, :message, :name, :created_at, :updated_at], :unique => true, :name => :all
+    t.index [:name, :created_at, :updated_at]
   end
 end
 
-class Article < ActiveRecord::Base
+class User < ActiveRecord::Base
 end
 
-Article.connection.indexes(Article.table_name).each do |e|
+User.connection.indexes(User.table_name).each do |e|
   p [e.name, e.columns]
 end
 
-# >> ["all", ["id", "message", "name", "created_at", "updated_at"]]
-# >> ["index_articles_on_id", ["id"]]
-# >> ["index_articles_on_name", ["name"]]
-# >> ["index_articles_on_message", ["message"]]
-# >> ["index_articles_on_message_and_name", ["message", "name"]]
+# >> ["index_users_on_name_and_created_at_and_updated_at", ["name", "created_at", "updated_at"]]
+# >> ["index_users_on_updated_at", ["updated_at"]]
+# >> ["index_users_on_created_at", ["created_at"]]
+# >> ["index_users_on_name", ["name"]]
