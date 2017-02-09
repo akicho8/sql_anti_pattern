1c1fc1d8eee7c64431096ef6d35aa220ceba1644
akicho8 <akicho8@gmail.com>
2017-02-09 23:02:40 +0900

    typeに入れるアレ→追加

---
+++ polymorphic_associations.rb
@@ -0,0 +1,38 @@
+require "active_record"
+require "rain_table"
+
+ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => ":memory:")
+ActiveRecord::Migration.verbose = false
+ActiveRecord::Schema.define do
+  create_table :users do |t|
+  end
+  create_table :comments do |t|
+    t.belongs_to :commentable, polymorphic: true
+  end
+end
+
+class User < ActiveRecord::Base
+  has_many :comments, as: :commentable
+end
+
+# あらゆるレコードにコメントできるモデル
+class Comment < ActiveRecord::Base
+  has_many :comments, as: :commentable # 自分に対してもコメントできるようにするため
+  belongs_to :commentable, polymorphic: true
+end
+
+user = User.create!             # => #<User id: 1>
+comment = user.comments.create! # => #<Comment id: 1, commentable_type: "User", commentable_id: 1>
+comment.comments.create!        # => #<Comment id: 2, commentable_type: "Comment", commentable_id: 1>
+comment = user.comments.create! # => #<Comment id: 3, commentable_type: "User", commentable_id: 1>
+comment.comments.create!        # => #<Comment id: 4, commentable_type: "Comment", commentable_id: 3>
+
+tt Comment.all.collect(&:attributes)
+# >> +----+------------------+----------------+
+# >> | id | commentable_type | commentable_id |
+# >> +----+------------------+----------------+
+# >> |  1 | User             |              1 |
+# >> |  2 | Comment          |              1 |
+# >> |  3 | User             |              1 |
+# >> |  4 | Comment          |              3 |
+# >> +----+------------------+----------------+
